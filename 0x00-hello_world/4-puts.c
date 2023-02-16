b9d54951f8563d9bce513137678f8647e75d8385
kelly green <kelzgreen1@gmail.com>
2023-02-16 11:51:21 -0800

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#!/bin/bash
+/**
+ *main- A program that uses puts to print a string
+ *Return: ends with 0
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
