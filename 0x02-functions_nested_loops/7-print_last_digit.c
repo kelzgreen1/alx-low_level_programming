ce889c2e4f6683118a7c9751e2a6ca4030b30bf0
kelly green <kelzgreen1@gmail.com>
2023-02-21 15:19:52 -0800

    task 8

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,15 +8,15 @@
 
 
 int print_last_digit(int a)
-{
-  int last_digit;
+{	
+  	int last_digit;
 
-  last_digit = a %  10;
-  if (last_digit < 0)
-  {
-    last_digit = last_digit * -1;
-  }
-  _putchar(last_digit + '0');
-  return (last_digit);
+ 	 last_digit = a %  10;
+  	if (last_digit < 0)
+ 	 {	
+   	 last_digit = last_digit * -1;	 
+	 }
+  	_putchar(last_digit + '0');
+  	return (last_digit);
 
 }
