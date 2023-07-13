3b98e161577d1473d1b6cbe93a8d6e945a95a1fb
kelly <kelzgreen1@gmail.com>
2023-07-13 13:02:45 -0700

    Updated files: 0x00-hello_world/4-puts.c 0x00-hello_world/a.out

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/** 
+ * this is my hello world program written in c
+ */
+
+int main(void)
+{
+char str[] = "Programming is like building a multilingual puzzle";
+puts(str);
+return(0);
+}
