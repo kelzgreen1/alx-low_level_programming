634628893acbb0ea57e907f2b2f64b4898cc376f
kelly <kelzgreen1@gmail.com>
2023-07-14 18:33:26 -0700

    Updated files: 0x01-variables_if_else_while/6-print_numberz 0x01-variables_if_else_while/6-print_numberz.c 0x01-variables_if_else_while/git

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int u;
+
+	u = 0;
+	while
+		(u < 10)
+{
+	putchar(u + '0');
+	u++;
+}
+putchar('\n');
+return (0);
+}
