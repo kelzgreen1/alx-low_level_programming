40c68d8a4007918fecb072dcf708397c477460f4
kelly <kelzgreen1@gmail.com>
2023-07-14 17:38:49 -0700

    task 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c;
+	char p;
+
+	c = 'a';
+
+	p = 'A';
+
+	while
+	(c <= 'z')
+	{
+	putchar(c);
+	c++;
+	}
+	while
+	(p <= 'Z')
+	{
+	putchar(p);
+	c++;
+	}
+putchar('\n');
+return (0);
+}
