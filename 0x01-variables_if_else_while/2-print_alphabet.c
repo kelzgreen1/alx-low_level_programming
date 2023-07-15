83f6d745bc9277f722087eca690c582159eefd0a
kelly <kelzgreen1@gmail.com>
2023-07-14 17:18:14 -0700

    task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - this code print the alphabet in lower caps
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c;
+
+	c = 'a';
+	while
+	(c <= 'z')
+	{
+	putchar(c);
+	c++;
+	}
+putchar('\n');
+return (0);
+}
