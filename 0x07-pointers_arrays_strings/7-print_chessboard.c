5e2eb0473ab311d6aa8d58ec59dc1ca56e9339c0
kelly <kelzgreen1@gmail.com>
2023-08-01 16:07:34 -0700

    Updated files: 0x07-pointers_arrays_strings/7-print_chessboard.c 0x05-pointers_arrays_strings/.1-swap.c.swp 0x05-pointers_arrays_strings/.2-strlen.c.swp

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_chessboard - prints the chessboard
+ * @a: pointer to the chessboard array
+ *
+ * Return: void
+ */
+void print_chessboard(char (*a)[8])
+{
+	int row, col;
+
+	for (row = 0; row < 8; row++)
+	{
+		for (col = 0; col < 8; col++)
+		{
+			putchar(a[row][col]);
+		}
+
+		putchar('\n');
+	}
+}
