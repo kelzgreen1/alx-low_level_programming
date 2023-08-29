b9199bd49fd89eac995df69e5eb839a05db1a4fc
kelly <kelzgreen1@gmail.com>
2023-08-29 16:00:45 -0700

    Updated files: 0x13-more_singly_linked_lists/3-add_nodeint_end.c README.md 0-print_name.c 0x05-pointers_arrays_strings/.1-swap.c.swp 0x05-pointers_arrays_strings/.2-strlen.c.swp function_pointers.h

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -9,9 +9,7 @@
  *
  * Return: The address of the new element, or NULL if it failed.
  */
-listint_t *add_nodeint_end(head, n)
-	listint_t **head;
-	const int n;
+listint_t *add_nodeint_end(listint_t **head, const int n)
 {
 	listint_t *new_node, *current;
 
