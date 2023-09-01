daf045083972177e89f0332e369486e266ddd8de
kelly <kelzgreen1@gmail.com>
2023-08-31 17:07:24 -0700

    Updated files: 0x14-bit_manipulation/100-get_endianness.c 0x14-bit_manipulation/100-main.c 0x14-bit_manipulation/h README.md 0-print_name.c 0x05-pointers_arrays_strings/.1-swap.c.swp 0x05-pointers_arrays_strings/.2-strlen.c.swp function_pointers.h

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,18 @@
+#include <stddef.h>
+#include "main.h"
+
+/**
+ * get_endianness - checks the endianness of the machine
+ *
+ * Return: 0 if big endian, 1 if little endian
+ */
+int get_endianness(void)
+{
+	unsigned int i = 1;
+	char *c = (char *)&i;
+
+	if (*c)
+		return (1);
+	else
+		return (0);
+}
