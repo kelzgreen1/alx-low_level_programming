0c718bc5c23d9d93fcc2075345d50362e498c4b8
kelly <kelzgreen1@gmail.com>
2023-09-06 14:53:56 -0700

    commit

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,34 +1,41 @@
-#include <unistd.h>
-#include <fcntl.h>
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
 
+/**
+ * read_textfile - function that reads a text file and prints it to
+ * the POSIX standard output
+ * @filename: file to read
+ * @letters: number of letters to be read and printed
+ * Return: the actual number of letters it could read and print
+ */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd;
-	ssize_t bytes_read = 0;
-	char buffer[1024];
+			int fd, printed, wrote;
+			char *buff;
 
-	if (!filename)
-		return 0;
+			buff = malloc(letters);
+			if (buff == NULL)
+			return (0);
 
-	fd = open(filename, O_RDONLY);
-	if (fd == -1)
-		return 0;
+			if (filename == NULL)
+			return (0);
 
-	while (letters > 0 && (bytes_read = read(fd, buffer, sizeof(buffer) - 1)) > 0)
-	{
-		buffer[bytes_read] = '\0';
-		if (write(STDOUT_FILENO, buffer, bytes_read) != bytes_read)
-		{
-			close(fd);
-			return 0;
-		}
-		letters -= bytes_read;
-	}
+			fd = open(filename, O_RDWR);
+			if (fd == -1)
+			return (0);
 
-	close(fd);
+			printed = read(fd, buff, letters);
+			if (printed == -1)
+			return (0);
 
-	if (bytes_read == -1)
-		return 0;
+			wrote = write(STDOUT_FILENO, buff, printed);
+			if (wrote == -1)
+			return (0);
 
-	return (ssize_t)(letters - bytes_read);
+			if (close(fd) == -1)
+			return (0);
+
+			free(buff);
+			return (printed);
 }
