7847b56b701ced58177d4df0b6992be98472c817
kelly <kelzgreen1@gmail.com>
2023-09-28 16:20:59 -0700

    Updated files: 0x14-bit_manipulation/0-binary_to_uint.c 0x14-bit_manipulation/0-main.c 0x14-bit_manipulation/1-main.c 0x14-bit_manipulation/1-print_binary.c 0x14-bit_manipulation/_putchar.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -10,19 +10,19 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-        unsigned int result = 0;
-        int i;
+	unsigned int result = 0;
+	int i;
 
-        if (b == NULL)
-                return (0);
+	if (b == NULL)
+	return (0);
 
-        for (i = 0; b[i] != '\0'; i++)
-        {
-                if (b[i] < '0' || b[i] > '1')
-                        return (0);
+	for (i = 0; b[i] != '\0'; i++)
+	{
+		if (b[i] < '0' || b[i] > '1')
+		return (0);
 
-                result = result * 2 + (b[i] - '0');
-        }
+		result = result * 2 + (b[i] - '0');
+	}
 
-        return (result);
+	return (result);
 }
